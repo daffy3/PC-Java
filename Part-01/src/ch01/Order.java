9486e9777d9962d16ba2857306966ad52fd8942e
daffy3 <weartstudio@naver.com>
2024-07-20 14:48:09 +0900

    Part 01. 객체지향 프로그래밍 - 객체지향 입문

---
+++ Part-01/src/ch01/Order.java
@@ -0,0 +1,9 @@
+package ch01;
+
+public class Order {
+    int orderId; // 주문번호
+    String buyerId; // 주문자 id
+    String sellerId; // 판매자 id
+    int productId; // 상품 id
+    String orderDate; // 주문 날짜
+}
