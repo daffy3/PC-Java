791cd1ea831d63cd56fa106e0aaf2d5c8f4ae526
daffy3 <weartstudio@naver.com>
2024-07-23 17:13:49 +0900

    Part 08. 스프링부트 API 만들어보기

---
+++ Part-08/src/main/java/com/example/part08/controller/PostApiController.java
@@ -0,0 +1,18 @@
+package com.example.part08.controller;
+
+import com.example.part08.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody PostRequestDto requestData) {
+        System.out.println(requestData);
+    }
+
+}
---
+++ Part-08/src/test/java/com/example/part08/Part08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.part08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Part08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
