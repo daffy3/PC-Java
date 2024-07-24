fd071e79343c78cdc7a60994a3be27498e4658c5
SON7AE <bobsot0225@gmail.com>
2024-07-24 17:05:58 +0900

    Part 10. 스프링의 기능 활용하기

--- Part-10/src/main/java/com/example/jpa/part10/dto/User.java
+++ Part-10/src/main/java/com/example/jpa/part10/dto/User.java
@@ -1,8 +1,9 @@
 package com.example.jpa.part10.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class User {
     private String name;
@@ -15,6 +16,14 @@ public class User {
 
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "휴대폰 번호의 양식이 맞지 않습니다.")
     private String phoneNumber;
+
+
+    @Size(min = 6, max = 6)
+    private String reqYearMonth; // yyyy-mm
+
+    public String getReqYearMonth() {
+        return reqYearMonth;
+    }
 
     public String getName() {
         return name;
@@ -48,6 +57,16 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
+    @AssertTrue(message = "yyyyMM 형식에 맞지 않습니다.")
+    public boolean isReqYearMonthValid() {
+        try {
+            LocalDate localDate = LocalDate.parse(getReqYearMonth() + "01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        } catch (Exception error) {
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -55,6 +74,7 @@ public class User {
                 ", age=" + age +
                 ", email='" + email + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
                 '}';
     }
 }
