975fd6f5de651b2f4af7ecbe62921d6f06aeaa76
SON7AE <bobsot0225@gmail.com>
2024-07-25 09:32:20 +0900

    Part 02. 객체지향 프로그래밍 - 객체지향 핵심

--- Part-02/src/ch05/Customer.java
+++ Part-02/src/ch05/Customer.java
@@ -16,7 +16,7 @@ public class Customer {
     }
 
     public int calcPrice(int price) {
-        bonusPoint += price + bonusRatio;
+        bonusPoint += price * bonusRatio;
         return price;
     }
 
