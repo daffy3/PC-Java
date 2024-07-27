9e595e366031b7035acbcce253751d731737f3a9
daffy3 <weartstudio@naver.com>
2024-07-27 16:08:24 +0900

    Part 10. 스프링의 기능 활용하기
    네이버 지역검색 API를 활용한 맛집 List 만들기

--- Part-10/rest-template/server/src/main/java/org/daffy3/server/controller/ServerApiController.java
+++ Part-10/rest-template/server/src/main/java/org/daffy3/server/controller/ServerApiController.java
@@ -5,13 +5,44 @@ import org.daffy3.server.dto.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
 
     private static final Logger log = LoggerFactory.getLogger(ServerApiController.class);
+
+
+    @GetMapping("/naver")
+    public String naver() {
+        String query = "용인";
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local")
+                .queryParam("query", query)
+                .queryParam("display", 100)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode(StandardCharsets.UTF_8).build().toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        RequestEntity<Void> req = RequestEntity.get(uri).header("X-Naver-Client-Id", "wKIApr24VMOaY1aSpzkc").header("X-Naver-Client-Secret", "4kN7PvIu9R").build();
+        ResponseEntity<String> response = restTemplate.exchange(req, String.class);
+
+        return response.getBody();
+    }
+
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age) {
