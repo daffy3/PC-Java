496dbf9f8ddcd6fba7be5d3d4cefff0a598457a5
daffy3 <weartstudio@naver.com>
2024-07-30 11:17:53 +0900

    Chapter11 - JPA 프로그래밍: 학습 전 기초 사항 Lombok에 대해서

---
+++ Chapter11/bookmanager/src/test/java/org/daffy3/jpa/bookmanager/BookmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daffy3.jpa.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
